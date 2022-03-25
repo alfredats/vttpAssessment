1e1a352fc1691aa95aadb18b2c715fad423e304c
Alfred A <20140832+alfredats@users.noreply.github.com>
2022-03-25 10:58:12 +0800

    up to task 7

--- src/main/java/sg/edu/nus/iss/vttpAssessment/controller/PurchaseOrderRestController.java
+++ src/main/java/sg/edu/nus/iss/vttpAssessment/controller/PurchaseOrderRestController.java
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.json.JsonStructure;
+import jakarta.json.JsonValue;
 import sg.edu.nus.iss.vttpAssessment.model.Quotation;
 import sg.edu.nus.iss.vttpAssessment.service.QuotationService;
 
@@ -30,7 +33,7 @@ public class PurchaseOrderRestController {
     private QuotationService qSvc;
 
     @PostMapping(path = "/po")
-    public ResponseEntity<String> indexResource(@RequestBody String reqBody) {
+    public ResponseEntity<String> poResource(@RequestBody String reqBody) {
         logger.info(">>> raw request: " + reqBody);
 
         JsonObject respJson = Json.createReader(new StringReader(reqBody)).readObject();
@@ -46,9 +49,32 @@ public class PurchaseOrderRestController {
 
         logger.info(">>> quotation items: " + items.toString());
         
-        Optional<Quotation> quot = qSvc.getQuotations(items);
+        Quotation quot = qSvc.getQuotations(items).orElse(null);
 
-        return null;
+        try {
+            logger.info(">>> Quotation successful: " + quot.getQuotations());
+            double total = 0;
+
+            for (JsonValue entry : lineitems) {
+                JsonObject x = entry.asJsonObject(); 
+                total += x.getInt("quantity") * quot.getQuotation(x.getString("item"));
+            }
+
+            JsonObjectBuilder respBuilder = Json.createObjectBuilder();
+            respBuilder.add("invoiceId", quot.getQuoteId())
+                       .add("name", respJson.getString("name"))
+                       .add("total", total);
+
+            return ResponseEntity.ok().body(respBuilder.build().toString());
+
+        } catch (NullPointerException e) {
+            logger.error("Quotation returned from Service was empty: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        
+        return ResponseEntity.status(400).body("{}");
+
     }
     
 }
--- src/test/java/sg/edu/nus/iss/vttpAssessment/VttpAssessmentApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/vttpAssessment/VttpAssessmentApplicationTests.java
@@ -1,13 +1,36 @@
 package sg.edu.nus.iss.vttpAssessment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import sg.edu.nus.iss.vttpAssessment.model.Quotation;
+import sg.edu.nus.iss.vttpAssessment.service.QuotationService;
 
 @SpringBootTest
 class VttpAssessmentApplicationTests {
 
+	@Autowired
+	private QuotationService qSvc;
+
 	@Test
 	void contextLoads() {
+	List<String> items = new ArrayList<>();
+	items.add("durian");
+	items.add("plum");
+	items.add("pear");
+
+	Optional<Quotation> optQuot = qSvc.getQuotations(items);
+
+	Assertions.assertFalse(optQuot.isPresent());
 	}
 
 }
